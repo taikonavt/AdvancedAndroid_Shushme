967a4decf8762093446b90b72cdff3ccbf2757da
taikonavt <taikonavt@gmail.com>
2017-11-10 17:49:49 +0300

    make 4th exercise

---
+++ app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package com.example.android.shushme;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by maxim on 10.11.17.
+ */
+
+public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String TAG = GeofenceBroadcastReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive called");
+    }
+}
